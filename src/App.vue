48a15674219082a8ff4543e5c9e99e0978301487
yanhan569046480 <569046480@qq.com>
2025-03-28 16:58:30 +0800

    加顶部路由菜单

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <template>
+  <el-menu mode="horizontal" router :default-active="$route.path" class="nav-menu">
+    <el-menu-item index="/caiZhengZiJinChengJieFenXi">财政资金承接分析</el-menu-item>
+    <el-menu-item index="/caiZhengYeWuKeHuYingXiaoFenXi">财政业务客户营销分析</el-menu-item>
+  </el-menu>
   <router-view></router-view>
 </template>
 
 <script setup>
 </script>
 
-<style></style>
+<style>
+.nav-menu {
+  padding: 0 20px;
+  margin-bottom: 20px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+</style>
